105e8e2cfda771b9055fc86c88ab6ae26152d685
Garry <igor_tarasov_84@mail.ru>
2022-07-20 19:27:19 +0500

    do homework task_4

--- DZ_4/Program.cs
+++ DZ_4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 2;
+while (x <= number)
+
+    if (x % 2 == 0 )
+    {
+    Console.WriteLine(x); 
+    x++;
+    }
+
