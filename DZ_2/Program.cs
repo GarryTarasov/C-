640f5d954792082611bef1cd572f984e36bf13c7
Garry <igor_tarasov_84@mail.ru>
2022-07-20 18:09:36 +0500

    end homework task_2

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -4,4 +4,34 @@
 //22 3 9 -> 22
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число (А)");
+int number_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число (B)");
+int number_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число (C)");
+int number_C = Convert.ToInt32(Console.ReadLine());
+int max=0;
+
+if (number_A >= number_B&&number_A>=number_C)
+{
+    max=number_A;
+}
+else if(number_B>=number_C&&number_B>=number_A)
+{
+    max=number_B;
+}
+else if(number_C>=number_A&&number_C>=number_B)
+{
+    max=number_C;
+}
+    {
+    Console.WriteLine("наибольшее число среди трёх "+ max);
+    }
+
+
+
+
+    
+        
+    
+      
