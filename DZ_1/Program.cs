7de342b2131cd937799af3f7225d179384412681
Garry <igor_tarasov_84@mail.ru>
2022-07-20 12:49:54 +0500

    bigen homework task_1/1

--- DZ_1/Program.cs
+++ DZ_1/Program.cs
@@ -1,3 +1,11 @@
-﻿// Задача №1
+﻿// Задача №1Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+//а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число (А)");
+int number_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число (B)");
+int number_B = Convert.ToInt32(Console.ReadLine());
+
